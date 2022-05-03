f5bf694d686ade66f5796dc1255a0cdbc4432c75
Mhayk Whandson <hi@mhayk.com>
2022-05-03 14:29:58 +0100

    feat: the first component called widget has been created
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
+import { Widget } from './components/Widget'
 
-function App() {
-  return <h1>Hello World</h1>
+export function App() {
+  return <Widget />
 }
-
-export default App
